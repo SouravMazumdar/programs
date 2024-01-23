a170b7926e4349fa07b4b51e60486cf542aa40d7
SouravMazumdar <152843690+SouravMazumdar@users.noreply.github.com>
2024-01-23 16:07:43 +0530

    Update program 5,c.cpp

--- program 5,c.cpp
+++ program 5,c.cpp
@@ -8,13 +8,11 @@ int main() {
     cout << "Enter an alphabet: ";
     cin >> c;
 
-    // evaluates to 1 (true) if c is a lowercase vowel
+    
     isLowercaseVowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
 
-    // evaluates to 1 (true) if c is an uppercase vowel
-    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-
-    // show error message if c is not an alphabet
+    isUppercaseVowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U
+        
     if (!isalpha(c))
       printf("Error! Non-alphabetic character.");
     else if (isLowercaseVowel || isUppercaseVowel)
