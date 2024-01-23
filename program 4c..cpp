84912b77a20ed713c16afbf237d8bcbaf4e36fd4
SouravMazumdar <152843690+SouravMazumdar@users.noreply.github.com>
2024-01-23 16:29:41 +0530

    Update program 4c..cpp

--- program 4c..cpp
+++ program 4c..cpp
@@ -1,20 +1,15 @@
-#include <iostream>
-using namespace std;
+#include<stdio.h>
+int main() {
+    int intType;
+    float floatType;
+    double doubleType;
+    char charType;
 
-int main()
-{
-    int a = 5, b = 10, temp;
-
-    cout << "Before swapping." << endl;
-    cout << "a = " << a << ", b = " << b << endl;
-
-    temp = a;
-    a = b;
-    b = temp;
-
-    cout << "\nAfter swapping." << endl;
-    cout << "a = " << a << ", b = " << b << endl;
-
+    printf("Size of int: %zu bytes\n", sizeof(intType));
+    printf("Size of float: %zu bytes\n", sizeof(floatType));
+    printf("Size of double: %zu bytes\n", sizeof(doubleType));
+    printf("Size of char: %zu byte\n", sizeof(charType));
+    
     return 0;
 }
 
